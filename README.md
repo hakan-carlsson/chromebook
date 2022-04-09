1666918c7f355a3069e8b989b3faad53a9e25e7f
Hakan Carlsson <hockey@daemon.se>
2022-04-09 11:24:24 +0200

    after upgrade to debian 11

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+Setup linux version:
+
+	https://www.aboutchromebooks.com/news/chrome-os-94-brings-debian-bullseye-to-linux-on-chromebooks/
+
+	chrome://flags#crostini-container-install
+	set to Bullseye / Debian 11
+
+Enable linux:
+
+	Settings / Advanced / Linux / Enable
+
+
+
--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 # install tools
 
-cp dot.code-special.sh ~/.code-special.sh
-
 bash code.sh
 sudo apt -y install dmidecode
 sudo apt -y install apt-utils
@@ -13,4 +11,18 @@ sudo apt -y install gnome-keyring
 sudo apt -y install python3-pip
 pip3 install python-dotenv
 sudo apt -y install jq
-# sudo apt -y install lxterminal
+sudo apt -y install tilix
+
+
+# add .code-special, manually add to .bashrc
+cp dot.code-special.sh ~/.code-special.sh
+
+# Add github config to ssh
+if [ ! -r ~/.ssh/config ] ;then
+    cat >> ~/.ssh/config << EOF
+
+Host github.com
+    HostName github.com
+    IdentityFile ~/.ssh/github
+
+EOF
